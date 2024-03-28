a5816501d2908d2b496b5d228664b66fec0cd1e1
Ludrachel <ludmylaraquel0501@gmail.com>
2024-03-28 18:15:46 -0300

    web finalizado

--- erickgym-web/script.js
+++ erickgym-web/script.js
@@ -1,3 +1,46 @@
+const lista = document.getElementById('alunos')
+
+function criarAluno(aluno) {
+    const li = document.createElement('li')
+    li.textContent = aluno
+    lista.appendChild(li)
+}
+
+async function postAluno(e) {
+    e.preventDefault()
+
+    const cx_nome = document.getElementById('nome')
+
+    const cx_idade = document.getElementById('idade')
+
+    const nome = cx_nome.value
+    const idade = Number(cx_idade.value)
+
+    if (nome === "") {
+        alert("Preecha todos os campos!")
+    }
+
+    const data = {nome, idade}
+
+    const response = await fetch('https://erickgym-0xkh.onrender.com/alunos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    if (response.status === 201) {
+        alert('Criado com sucesso!')
+        cx_nome.value = ""
+        cx_idade.value = ""
+        const aluno = await response.json()
+        criarAluno(aluno.nome)
+    } else {
+        alert("Deu ruim")
+    }
+}
+
 async function getAlunos() {
     const resposta = await fetch('https://erickgym-0xkh.onrender.com/alunos')
 
@@ -10,7 +53,9 @@ async function getAlunos() {
 async function principal() {
     const alunos = await getAlunos()
 
-    console.log(alunos)
+    alunos.map((aluno) => criarAluno(aluno.nome))
 }
 
 principal()
+
+document.getElementById('form').addEventListener('submit', postAluno)
